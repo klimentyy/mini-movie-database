2bfbd9502455a1da0d9e5656a4a68225a832d9d1
Klim Shmelev <klimenty.shmelev@gmail.com>
2026-06-23 21:00:31 +0200

    Add migrations for the model

--- movies/models.py
+++ movies/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
 class Actor(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Movie(models.Model):
     cz_title = models.CharField(max_length=255)
-    original_title = models.CharField(max_length=255, null=True, blank=True)
+    en_title = models.CharField(max_length=255, null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name="movies")
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cz_title", "en_title"], name="unique_movie_titles")
+        ]
+
     def __str__(self):
-        return self.title
+        return self.cz_title
