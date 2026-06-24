10fd8bd326d500521b3eeb8562d818534332f266
Klim Shmelev <klimenty.shmelev@gmail.com>
2026-06-24 22:43:34 +0200

    Add view endpoints to render a template

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.db.models import F, Func, Q
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from unidecode import unidecode
 from .models import Movie, Actor
 
@@ -8,8 +8,18 @@ from .models import Movie, Actor
 class RemoveAccent(Func):
     function = "remove_accents"
 
+def search_page_view(request):
+    return render(request, 'movies/search.html')
 
-def search_view(request):
+def movie_detail_view(request, pk):
+    movie = get_object_or_404(Movie.objects.prefetch_related("actors"), pk=pk)
+    return render(request, "movies/movie_detail.html", {"movie": movie})
+
+def actor_detail_view(request, pk):
+    actor = get_object_or_404(Actor.objects.prefetch_related("movies"), pk=pk)
+    return render(request, "movies/actor_detail.html", {"actor": actor})
+
+def search_api_view(request):
     query = request.GET.get("q", "").strip()
 
     if len(query) < 2:
@@ -33,13 +43,14 @@ def search_view(request):
         Actor.objects.annotate(normalized_name=RemoveAccent(F("name")))
         .filter(normalized_name__contains=clean_query)
         .distinct()
+        .values("id", "name")
     )
 
     return JsonResponse(
         {
             "results": {
-                "movies": list(movies.values("id", "cz_title", "en_title")),
-                "actors": list(actors.values("id", "first_name", "last_name")),
+                "movies": list(movies),
+                "actors": list(actors),
             }
         }
     )
