818be2bc95b6dfd66020b141c9ce773ac12a62bb
Klim Shmelev <klimenty.shmelev@gmail.com>
2026-06-25 17:40:49 +0200

    Improve code formatting and api response

--- movies/views.py
+++ movies/views.py
@@ -8,22 +8,26 @@ from .models import Movie, Actor
 class RemoveAccent(Func):
     function = "remove_accents"
 
+
 def search_page_view(request):
-    return render(request, 'movies/search.html')
+    return render(request, "movies/search.html")
+
 
 def movie_detail_view(request, pk):
     movie = get_object_or_404(Movie.objects.prefetch_related("actors"), pk=pk)
     return render(request, "movies/movie_detail.html", {"movie": movie})
 
+
 def actor_detail_view(request, pk):
     actor = get_object_or_404(Actor.objects.prefetch_related("movies"), pk=pk)
     return render(request, "movies/actor_detail.html", {"actor": actor})
+
 
 def search_api_view(request):
     query = request.GET.get("q", "").strip()
 
     if len(query) < 2:
-        return JsonResponse({"results": []})
+        return JsonResponse({"results": {"movies": [], "actors": []}})
 
     movies = []
     actors = []
